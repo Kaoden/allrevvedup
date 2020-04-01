b43d7eff3f70d01a8ecdefe2e68dc12210aabcd9
Kaoden <kevin.mullins89@gmail.com>
2020-04-01 17:04:15 -0400

    Created angular app, placed into new local_apps folder. Added angular app to base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "allrevvedup.users.apps.UsersConfig",
+    "local_apps.angular",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
